b71fff6aa92dd9a3a866f6b89bb1e8ade6757125
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2019-02-24 15:00:25 +0100

    init commit for project NodeJs Express Crach Course

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'Yassine Hadachi',
+    email: 'yass@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2, 
+    name: 'Hamza Abousaid',
+    email: 'mizo@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3, 
+    name: 'Mehdi El Bejjaji',
+    email: 'mido@gmail.com',
+    status: 'active'
+  }
+]; 
+
+module.exports = members;
